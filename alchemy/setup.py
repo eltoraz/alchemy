0f02f32b295b531d72e70cfe63ab20f44eee5509
Bill Jameson <williamjameson@alum.rpi.edu>
2015-05-30 22:08:19 -0400

    Some scaffolding
    Some stuff I'll need later when I actually want to distribute this. Just adding
    it now so I can say I made some progress today...

---
+++ alchemy/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+config = {
+    'name': 'alchemy',
+    'description': 'Potion-crafting game',
+    'version': '0.1',
+    'author': 'eltoraz',
+    'author_email': 'eltoraz@outlook.com'
+    'url': 'https://github.com/eltoraz/alchemy',
+    'packages': find_packages(),
+    'install_requires': []
+}
+
+setup(**config)
